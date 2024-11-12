8325884cd8c0eeb596c2c2a07f96158bbbce6bec
Harold Mbarga <mbah00@hotmail.com>
2024-11-12 04:04:42 -0500

    created jenkins-vm

--- terraform/resources/devlopment/ec2-instance/main.tf
+++ terraform/resources/devlopment/ec2-instance/main.tf
@@ -29,17 +29,4 @@ locals {
 
 module "ec2_instance" {
   source      = "../../../modules/ec2-instance"
-  # key_name    = var.key_name
-  # # ami_id      = data.aws_ami.jenkins_master_ami.id
-  # aws_region  = local.aws_region
-  # common_tags = local.common_tags
-}
 
-# # Output to retrieve instance details
-# output "instance_public_ip" {
-#   value = module.ec2_instance.instance_public_ip
-# }
-
-# output "vpc_id" {
-#   value = module.ec2_instance.vpc_id
-# }
